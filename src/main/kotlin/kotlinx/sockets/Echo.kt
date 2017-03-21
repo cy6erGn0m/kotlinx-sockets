7dff49226356a027343ea8bd9f9da6b5c56e25ce
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-03-21 11:09:12 +0300

    echo example

---
+++ src/main/kotlin/kotlinx/sockets/Echo.kt
@@ -0,0 +1,37 @@
+package kotlinx.sockets
+
+import kotlinx.coroutines.experimental.*
+import java.net.*
+import java.nio.*
+
+fun main(args: Array<String>) {
+    runBlocking {
+        SelectorManager().use { selector ->
+            selector.serverSocket().use { server ->
+                server.bind(InetSocketAddress(9094))
+
+                while (true) {
+                    runClient(server.accept())
+                }
+            }
+        }
+    }
+}
+
+private fun runClient(client: AsyncSocket) {
+    launch(CommonPool) {
+        client.use {
+            val bb = ByteBuffer.allocateDirect(8192)
+            while (true) {
+                bb.clear()
+                val rc = client.read(bb)
+                if (rc == -1) break
+                bb.flip()
+
+                while (bb.hasRemaining()) {
+                    client.write(bb)
+                }
+            }
+        }
+    }
+}
