6169909cac27080e04199ce30363c88f7487491a
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-03-23 20:18:39 +0300

    simple pool pre-filler

---
+++ kotlin-sockets/src/main/kotlin/kotlinx/sockets/DefaultPool.kt
@@ -0,0 +1,28 @@
+package kotlinx.sockets
+
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.channels.*
+import java.nio.*
+import java.util.concurrent.*
+
+fun runDefaultByteBufferPool(capacity: Int): Channel<ByteBuffer> {
+    return runDefaultByteBufferPool(ArrayChannel<ByteBuffer>(capacity))
+}
+
+fun runDefaultByteBufferPool(pool: Channel<ByteBuffer>): Channel<ByteBuffer> {
+    launch(CommonPool) {
+        while (true) { // initial fill
+            if (!pool.offer(ByteBuffer.allocate(8192))) break
+        }
+
+        while (true) {
+            delay(10L, TimeUnit.SECONDS)
+
+            while (true) { // add more
+                if (!pool.offer(ByteBuffer.allocate(8192))) break
+            }
+        }
+    }
+
+    return pool
+}
