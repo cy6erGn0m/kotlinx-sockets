5f62a6d04c548dfe462aa5288e13603c5bbd37be
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-03-26 21:00:19 +0300

    Make io dispatcher visible

--- kotlin-sockets/src/main/kotlin/kotlinx/sockets/Pools.kt
+++ kotlin-sockets/src/main/kotlin/kotlinx/sockets/Pools.kt
@@ -8,11 +8,11 @@ private val group = ThreadGroup("io-pool-group")
 private val counter = AtomicInteger()
 private val cpuCount = Runtime.getRuntime().availableProcessors()
 
-internal val ioPool = ThreadPoolExecutor(cpuCount + 1, cpuCount * 2 + 1, 10L, TimeUnit.SECONDS, ArrayBlockingQueue<Runnable>(100000), { r ->
+private val ioPool = ThreadPoolExecutor(cpuCount + 1, cpuCount * 2 + 1, 10L, TimeUnit.SECONDS, ArrayBlockingQueue<Runnable>(100000), { r ->
     Thread(group, r, group.name + counter.incrementAndGet()).apply {
         isDaemon = true
     }
 })
 
-internal val ioCoroutineDispatcher = ioPool.asCoroutineDispatcher()
+val ioCoroutineDispatcher = ioPool.asCoroutineDispatcher()
 
