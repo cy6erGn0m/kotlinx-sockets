78248eab8a23ec974066a6aa2d594f6cd1828207
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-08-31 17:28:53 +0300

    Custom coroutine dispatcher

--- kotlin-sockets/src/main/kotlin/kotlinx/sockets/Pools.kt
+++ kotlin-sockets/src/main/kotlin/kotlinx/sockets/Pools.kt
@@ -1,19 +1,21 @@
 package kotlinx.sockets
 
 import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.channels.*
 import kotlinx.coroutines.experimental.io.*
 import kotlinx.coroutines.experimental.io.internal.*
 import kotlinx.sockets.impl.ObjectPool
 import kotlinx.sockets.impl.ObjectPoolImpl
 import java.util.concurrent.*
 import java.util.concurrent.atomic.*
+import kotlin.coroutines.experimental.*
 
-val ioGroup = ThreadGroup("io-pool-group")
+val ioThreadGroup = ThreadGroup("io-pool-group")
 val selectorsGroup = ThreadGroup("selector-pool-group")
 
 private val cpuCount = Runtime.getRuntime().availableProcessors()
 
-val ioCoroutineDispatcher = IOCoroutineDispatcher(maxOf(2, (cpuCount * 2 / 3)))
+val ioCoroutineDispatcher: CoroutineDispatcher = IOCoroutineDispatcher(maxOf(2, (cpuCount * 2 / 3)))
 
 private val selectorsPool = ThreadPoolExecutor(
         0, Int.MAX_VALUE,
@@ -47,5 +49,46 @@ internal class DirectByteBufferPool(val bufferSize: Int, size: Int) : ObjectPool
     override fun validateInstance(instance: ByteBuffer) {
         require(instance.isDirect)
         require(instance.capacity() == bufferSize)
+    }
+}
+
+internal class IOCoroutineDispatcher(val nThreads: Int) : CoroutineDispatcher() {
+    private val dispatcherThreadGroup = ThreadGroup(ioThreadGroup, "io-pool-group-sub")
+    private val tasks = Channel<Runnable>(Channel.UNLIMITED)
+
+    init {
+        require(nThreads > 0) { "nThreads should be positive but $nThreads specified"}
+    }
+
+    private val threads = (1..nThreads).map {
+        IOThread().apply { start() }
+    }
+
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        if (!tasks.offer(block)) {
+            CommonPool.dispatch(context, block)
+        }
+    }
+
+    private inner class IOThread : Thread(dispatcherThreadGroup, "io-thread") {
+        init {
+            isDaemon = true
+        }
+
+        override fun run() {
+            runBlocking {
+                while (true) {
+                    val task = tasks.receiveOrNull() ?: break
+                    run(task)
+                }
+            }
+        }
+
+        private fun run(task: Runnable) {
+            try {
+                task.run()
+            } catch (t: Throwable) {
+            }
+        }
     }
 }
